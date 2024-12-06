a81964dfe46101f9a3a104855a7011bd0f616c80
Furiza31 <hugo.wendjaneh@gmail.com>
2024-12-06 17:11:32 +0100

    tp

---
+++ build/CMakeFiles/train.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/AbstractLocomotive.cpp" "CMakeFiles/train.dir/AbstractLocomotive.cpp.o" "gcc" "CMakeFiles/train.dir/AbstractLocomotive.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/AbstractWagon.cpp" "CMakeFiles/train.dir/AbstractWagon.cpp.o" "gcc" "CMakeFiles/train.dir/AbstractWagon.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/ElectricLocomotive.cpp" "CMakeFiles/train.dir/ElectricLocomotive.cpp.o" "gcc" "CMakeFiles/train.dir/ElectricLocomotive.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/FreightWagon.cpp" "CMakeFiles/train.dir/FreightWagon.cpp.o" "gcc" "CMakeFiles/train.dir/FreightWagon.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/GasolineLocomotive.cpp" "CMakeFiles/train.dir/GasolineLocomotive.cpp.o" "gcc" "CMakeFiles/train.dir/GasolineLocomotive.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/PassengerWagon.cpp" "CMakeFiles/train.dir/PassengerWagon.cpp.o" "gcc" "CMakeFiles/train.dir/PassengerWagon.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/Train.cpp" "CMakeFiles/train.dir/Train.cpp.o" "gcc" "CMakeFiles/train.dir/Train.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/TypeLocomotive.cpp" "CMakeFiles/train.dir/TypeLocomotive.cpp.o" "gcc" "CMakeFiles/train.dir/TypeLocomotive.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/TypeWagon.cpp" "CMakeFiles/train.dir/TypeWagon.cpp.o" "gcc" "CMakeFiles/train.dir/TypeWagon.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/UtilityWagon.cpp" "CMakeFiles/train.dir/UtilityWagon.cpp.o" "gcc" "CMakeFiles/train.dir/UtilityWagon.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/Wagons.cpp" "CMakeFiles/train.dir/Wagons.cpp.o" "gcc" "CMakeFiles/train.dir/Wagons.cpp.o.d"
+  "/home/furiza/school/ProgrammationCEtCpp/simasciitrain-4018/main.cpp" "CMakeFiles/train.dir/main.cpp.o" "gcc" "CMakeFiles/train.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/train.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/train.dir/AbstractLocomotive.cpp.o"
+  "CMakeFiles/train.dir/AbstractLocomotive.cpp.o.d"
+  "CMakeFiles/train.dir/AbstractWagon.cpp.o"
+  "CMakeFiles/train.dir/AbstractWagon.cpp.o.d"
+  "CMakeFiles/train.dir/ElectricLocomotive.cpp.o"
+  "CMakeFiles/train.dir/ElectricLocomotive.cpp.o.d"
+  "CMakeFiles/train.dir/FreightWagon.cpp.o"
+  "CMakeFiles/train.dir/FreightWagon.cpp.o.d"
+  "CMakeFiles/train.dir/GasolineLocomotive.cpp.o"
+  "CMakeFiles/train.dir/GasolineLocomotive.cpp.o.d"
+  "CMakeFiles/train.dir/PassengerWagon.cpp.o"
+  "CMakeFiles/train.dir/PassengerWagon.cpp.o.d"
+  "CMakeFiles/train.dir/Train.cpp.o"
+  "CMakeFiles/train.dir/Train.cpp.o.d"
+  "CMakeFiles/train.dir/TypeLocomotive.cpp.o"
+  "CMakeFiles/train.dir/TypeLocomotive.cpp.o.d"
+  "CMakeFiles/train.dir/TypeWagon.cpp.o"
+  "CMakeFiles/train.dir/TypeWagon.cpp.o.d"
+  "CMakeFiles/train.dir/UtilityWagon.cpp.o"
+  "CMakeFiles/train.dir/UtilityWagon.cpp.o.d"
+  "CMakeFiles/train.dir/Wagons.cpp.o"
+  "CMakeFiles/train.dir/Wagons.cpp.o.d"
+  "CMakeFiles/train.dir/main.cpp.o"
+  "CMakeFiles/train.dir/main.cpp.o.d"
+  "train"
+  "train.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
